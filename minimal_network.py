c2406527395340581467dbddb69a1f25a60331d0
clementlelievre <clement.lelievre90@gmail.com>
2023-04-28 16:51:42 +0200

    solved minimal network puzzle (after checking this is Kruskal's or Prim's algo)

--- minimal_network.py
+++ minimal_network.py
@@ -5,10 +5,9 @@ removing redundant edges whilst ensuring that the network remains connected.
 """
 import logging
 import os
-
-logging.basicConfig(
-    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
-)
+from collections import deque
+from copy import deepcopy
+from itertools import combinations
 
 TEST_MATRIX = """
 -,16,12,21,-,-,-
@@ -27,19 +26,19 @@ class NetworkOptimizer:
     while maximizing the saving on the weights"""
 
     def __init__(self, matrix: str) -> None:
-        # check if this is a file path
+        # check if `matrix` is a filepath
         if os.path.isfile(matrix):
             with open(matrix, "r") as f:
-                self.matrix = [
+                self.data = [
                     row.strip().split(",") for row in f.readlines() if row.strip()
                 ]
         else:
-            self.matrix = [
+            self.data = [
                 row.strip().split(",") for row in matrix.splitlines() if row.strip()
             ]
-        self.matrix = [
+        self.matrix: list[int | None] = [
             [int(elem) if elem.isnumeric() else None for elem in row]
-            for row in self.matrix
+            for row in self.data
         ]
         # make sure the matrix is squared
         assert len(self.matrix[0]) == len(self.matrix)
@@ -51,36 +50,71 @@ class NetworkOptimizer:
             sum(len(list((filter(None, row)))) for row in self.matrix) // 2
         )
         self.nb_vertices = len(self.matrix)
-        self.graph = {i: {} for i in range(self.nb_vertices)}
+        self.graph: dict[int, dict[int, int]] = {i: {} for i in range(self.nb_vertices)}
         logging.info(
             f"Initial total weight: {self.total_weight_initial}, initial number of edges: {self.nb_edges_initial}, number of vertices: {self.nb_vertices}"
         )
 
     def _build_graph(self) -> None:
         logging.info("Building graph...")
-        for i in range(self.nb_vertices):
-            for j in range(self.nb_vertices):
-                if i == j or self.matrix[i][j] is None:
-                    continue
-                self.graph[i][j] = self.matrix[i][j]
+        self.edges_initial = set()
+        for i, j in combinations(range(self.nb_vertices), 2):
+            if self.matrix[i][j] is None:
+                continue
+            self.graph[i][j] = (dist := self.matrix[i][j])
+            self.graph[j][i] = dist
+            self.edges_initial.add((dist, frozenset((i, j))))
         assert len(self.graph) == self.nb_vertices
         assert sum(map(len, self.graph.values())) // 2 == self.nb_edges_initial
+        self.edges_initial = sorted(self.edges_initial, reverse=True)
         logging.info("Graph built.")
+        logging.debug(f"Initial edges: {self.edges_initial}")
+        logging.debug(f"# of initial edges: {len(self.edges_initial)}")
+
+    @staticmethod
+    def _path_exists_between(
+        edge1: int, edge2: int, graph: dict[int, dict[int, int]]
+    ) -> bool:
+        queue = deque([edge1])
+        seen = set()
+        while queue:
+            current_edge = queue.pop()
+            if current_edge in seen:
+                continue
+            if current_edge == edge2:
+                return True
+            seen.add(current_edge)
+            queue.extend([neigh for neigh in graph[current_edge]])
+        return False
 
     def make_minimal_graph(self) -> int:
         """Prunes the graph while retaining all initial connections
+        Starts from the heaviest edges and drops them if the graph remains connected
 
         Returns:
             int: the maximum weight saving possible
         """
         self._build_graph()
-        max_saving = ...
+        max_saving = 0
+        temp_graph = deepcopy(self.graph)
+        for dist, vertices in self.edges_initial:
+            edge1, edge2 = vertices
+            # drop the edge and check if the graph is still connected
+            del temp_graph[edge1][edge2]
+            del temp_graph[edge2][edge1]
+            if NetworkOptimizer._path_exists_between(edge1, edge2, temp_graph):
+                max_saving += dist
+            else:  # re-add the edge
+                temp_graph[edge1][edge2] = temp_graph[edge2][edge1] = dist
+        logging.debug(f"Final graph: {temp_graph}")
         logging.info(f"Maximum saving: {max_saving}")
         return max_saving
 
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, format="%(asctime)s %(message)s")
     NO_test = NetworkOptimizer(TEST_MATRIX)
     assert NO_test.make_minimal_graph() == 150
+
     NO = NetworkOptimizer("minimal_network.txt")
     NO.make_minimal_graph()
