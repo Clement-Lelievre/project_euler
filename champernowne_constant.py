62ded18aff91da913ede8b2c3c6df4d9bb1a78f5
clementlelievre <clement.lelievre90@gmail.com>
2023-07-11 17:42:56 +0200

    correct algo

--- champernowne_constant.py
+++ champernowne_constant.py
@@ -1,43 +1,73 @@
 """https://projecteuler.net/problem=40"""
+import time
 from functools import reduce
 
 
-def solve_naive() -> None:
-    s = ""
-    for i in range(1, 1_000_001):
-        s += str(i)
-    assert s[11] == "1"
-    s = tuple(map(int, s))
+def solve_naive(max_pow: int = 6) -> None:
+    starttime = time.time()
+    s = "".join(map(str, range(1, 10**max_pow + 1)))
     print(
-        f"Naive solution: {reduce(lambda x, y: x * y, map(s.__getitem__, (10**i-1 for i in range(7))))}"
+        f"Naive solution: {reduce(lambda x, y: int(x) * int(y), map(s.__getitem__, (10**i-1 for i in range(max_pow+1))))} in {time.time() - starttime} seconds"
     )
 
 
-def get_char(ind: int) -> int:
-    # needs checking, currently wrong, should be quite close though
-    if ind == 0:
-        return 1
-    n = 0
-    i = 1
-    while n < ind:
-        n += i * 9 * 10 ** (i - 1)
-        i += 1
-    i -= 1
-    low = n - i * 9 * 10 ** (i - 1)
-    integer_part, remainder = divmod(ind - low + 1, i + 1)
-    return (
-        int(str(10**i + integer_part - 1)[remainder - 1])
-        if remainder == 0
-        else int(str(10**i + integer_part)[remainder])
+def get_char(n: int) -> int:
+    """The core algo.
+
+    Args:
+        n (int): the index of the digit to return. 0-based (ie takes into account the 0 before the dot in the constant)
+
+    Returns:
+        int: the digit at index n in Champernowne's constant
+    """
+    if n <= 9:
+        return n
+    consumed_indices = 10
+    width = 2
+    while (
+        True
+    ):  # a 'batch' means a group of numbers of the same width (e.g. 100 through 999 included is a batch)
+        if (
+            consumed_indices
+            + (next_batch_nb_of_indices := width * 9 * 10 ** (width - 1))
+            < n
+        ):
+            consumed_indices += next_batch_nb_of_indices
+            width += 1
+        else:
+            break
+    nb_ind_diff = (
+        n - consumed_indices + 1
+    )  # how many indices separate n from the first index of the latest batch
+    nb_full_numbers, remainder = divmod(nb_ind_diff, width)
+    return int(
+        str(10 ** (width - 1) + nb_full_numbers - (0 if remainder else 1))[
+            remainder - 1
+        ]
     )
 
 
-def solve_smarter() -> None:
+def solve_smarter(max_pow: int = 6) -> None:
+    """A much better algorithm than the naive one.
+
+    Args:
+        max_pow (int, optional): the max power used in the computations (e.g. d100000). Defaults to 6 which corresponds to the text of the problem.
+    """
+    starttime = time.time()
     print(
-        f"Smarter solution: {reduce(lambda x, y: x * y, map(get_char, (10**i - 1 for i in range(7))))}"
+        f"Smarter solution: {reduce(lambda x, y: x * y, map(get_char, (10**i for i in range(max_pow+1))))} in {time.time() - starttime} seconds"
     )
 
 
 if __name__ == "__main__":
+    for i in range(10):
+        assert get_char(i) == i
+    assert get_char(12) == 1 and get_char(13) == 1 and get_char(14) == 1
+    assert (
+        get_char(15) == 2
+        and get_char(16) == 1
+        and get_char(17) == 3
+        and get_char(18) == 1
+    )
     solve_naive()
     solve_smarter()
